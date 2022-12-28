c87a41764abf4d1ea858bd824d74a05a4f4575ab
Joel Azemar <joel.azemar@gmail.com>
2022-12-28 17:13:44 +0100

    Update exporter.rb

--- lib/csvbuilder/exporter.rb
+++ lib/csvbuilder/exporter.rb
@@ -4,10 +4,10 @@ require "csvbuilder/exporter/version"
 
 require "csv"
 require "ostruct"
-# require "active_model"
 
 require "active_support"
 require "active_support/dependencies/autoload"
+
 require "active_support/core_ext/object"
 require "active_support/core_ext/string"
 
